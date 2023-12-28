2e6a899003e312c157d865b23e37d962ae68237b
Mihir Patel <154884845+mihirvegad@users.noreply.github.com>
2023-12-28 10:41:30 +0530

    Update VApplication.java

--- src/main/java/com/codersfarm/demo/VApplication.java
+++ src/main/java/com/codersfarm/demo/VApplication.java
@@ -6,16 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VApplication {
-	  @GetMapping("/")
-		public String v() {
-		return "hurrey!! Deployee succesfully";
-	}
-	@GetMapping("/login")
-	public String v() {
-	return "login";
-}
 	public static void main(String[] args) {
 		SpringApplication.run(VApplication.class, args);
 	}
